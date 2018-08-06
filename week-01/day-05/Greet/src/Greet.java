5be6a67720ca7f51708c60a80692b3b436ed149c
SomodiEmese <somodi.emese24@gmail.com>
2018-08-06 14:50:37 +0200

    add Greet to repo

---
+++ week-01/day-05/Greet/src/Greet.java
@@ -0,0 +1,15 @@
+public class Greet {
+    public static void main(String[] args){
+
+        String al = "Greenfox";
+        System.out.println(greet(al));
+    }
+
+    public static String greet(String al){
+        return "Greetings dear, "+ al;
+    }
+
+
+
+
+}
