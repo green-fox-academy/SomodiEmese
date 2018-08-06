6ab562fef853d983d007eb09a43a76491e9b0c8c
SomodiEmese <somodi.emese24@gmail.com>
2018-08-06 16:47:44 +0200

    add IncrementElement to repo

---
+++ week-01/day-05/IncrementElement/src/IncrementElement.java
@@ -0,0 +1,10 @@
+public class IncrementElement {
+    public static void main(String[] args){
+
+        int[] t = new int[]{1, 2, 3, 4, 5};
+        t[2] += 2;
+
+        System.out.println(t[2]);
+
+    }
+}
