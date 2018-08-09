9aa19ac41de1e424be8fd47c0ad45c845fbd35dc
SomodiEmese <somodi.emese24@gmail.com>
2018-08-09 20:53:34 +0200

    add Project-01 to repo

--- week-02/day-09/Project-01/src/Project01.java
+++ week-02/day-09/Project-01/src/Project01.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class Project01 {
+ public class Project01 {
+    public static void mainDraw(Graphics graphics) {
+        graphics.setColor(Color.green);
+
+        for (int i = 0; i < 16 ; i++){
+            int lineGreen = i * 20;
+            graphics.drawLine(0,lineGreen,lineGreen, 320);
+        }
+        graphics.setColor(Color.red);
+
+        for (int i = 0; i < 16; i++){
+            int lineRed = 320 - i * 20;
+            graphics.drawLine(lineRed, 0,320, lineRed);
+        }
+    }
+
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
 }
