451444d7e7cc618fb2d6685ecc91f46a48a9fd72
SomodiEmese <somodi.emese24@gmail.com>
2018-08-16 15:20:06 +0200

    add DiceSet to repo

--- week-03/day-13/DiceSet/src/DiceSet.java
+++ week-03/day-13/DiceSet/src/DiceSet.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class DiceSet {
+
+    int[] dices = new int[6];
+
+    public int[] roll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+        return dices;
+    }
+
+    public int[] getCurrent() {
+        return dices;
+    }
+
+    public int getCurrent(int i) {
+        return dices[i];
+    }
+
+    public void reroll() {
+        for (int i = 0; i < dices.length; i++) {
+            dices[i] = (int) (Math.random() * 6) + 1;
+        }
+    }
+
+    public void reroll(int k) {
+        dices[k] = (int) (Math.random() * 6) + 1;
+    }
+
+    public static void main(String[] args) {
+        DiceSet diceSet = new DiceSet();
+        diceSet.roll();
+        boolean isAllSix;
+
+        do {
+            isAllSix = true;
+            for (int i = 0; i < diceSet.dices.length; i++) {
+                if (diceSet.getCurrent(i) != 6) {
+                    diceSet.reroll(i);
+                    isAllSix = false;
+                }
+            }
+        } while (!isAllSix);
+
+        for (int i = 0; i < diceSet.dices.length; i++) {
+            System.out.println(diceSet.getCurrent(i));
+        }
+    }
 }
