e66182e86854e26f2e81d1e09e4ca50ab90c81db
SomodiEmese <somodi.emese24@gmail.com>
2018-08-19 19:00:10 +0200

    add WrtieSingleLine to repo

---
+++ week-04/WriteSingleLine/src/WriteSingleLine.java
@@ -0,0 +1,22 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WriteSingleLine {
+    public static void main(String[] args) {
+        firstLine();
+    }
+
+    private static void firstLine() {
+        List<String> content = new ArrayList<>();
+        content.add("Somodi Emese");
+        try {
+            Path filePath = Paths.get("my-file.txt");
+            Files.write(filePath, content);
+        } catch (Exception e) {
+            System.out.println("Unable to write file: my-file.txt");
+        }
+    }
+}
