a4c70e88fa1fbc51fe5ec7c3cd472e1c1bb4f62f
Unknown <somodi.emese24@gmail.com>
2018-09-19 17:26:15 +0200

    add some week7 monday tasks

--- week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloSytle.java
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloSytle.java
@@ -1,4 +1,26 @@
 package com.greenfoxacademy.helloworld.controllers;
 
+import java.awt.*;
+import java.util.Random;
+
 public class HelloSytle {
+
+
+    public Color randomColor() {
+        Random randomGenerator = new Random();
+        Integer red = randomGenerator.nextInt(256);
+        Integer green = randomGenerator.nextInt(256);
+        Integer blue = randomGenerator.nextInt(256);
+
+        return new Color(red, green, blue);
+    }
+
+    public Font randomSize () {
+        int z = (int)(Math.random() * 300 - 0);
+        String[] fontNames = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        int index = (int)(Math.random() * (fontNames.length - 1));
+        String fontName = fontNames[index];
+
+        return new Font(fontName, Font.PLAIN, z);
+    }
 }
--- week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloWebController.java
+++ week-07/helloworld/src/main/java/com/greenfoxacademy/helloworld/controllers/HelloWebController.java
@@ -3,6 +3,7 @@ package com.greenfoxacademy.helloworld.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -10,9 +11,9 @@ import java.util.concurrent.atomic.AtomicLong;
 public class HelloWebController {
     AtomicLong atomicLong = new AtomicLong(0);
 
-    @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    @RequestMapping("/web/greeting5")
+    public String greeting(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", " Emese");
         model.addAttribute("atomicLong", atomicLong.incrementAndGet() );
         return "greeting";
     }
